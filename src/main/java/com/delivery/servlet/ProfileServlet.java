0347b075f2faff36a0ef02c91247fc9a73e20c84
DESKTOP-6PMIQ8Q\kotsk <kotsko10@gmail.com>
2021-09-29 20:13:14 +0300

    add ProfileServlet

--- src/main/java/com/delivery/servlet/ProfileServlet.java
+++ src/main/java/com/delivery/servlet/ProfileServlet.java
@@ -9,16 +9,30 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.*;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/profile")
 public class ProfileServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         User user = (User) req.getSession().getAttribute("user");
-        List<Order> orders = OrderDao.getInstance().getOrderByUserId(user.getId());
-        req.setAttribute("orders",orders);
-        req.getRequestDispatcher("/profile.jsp").forward(req,resp);
+        int page = 1;
+        int total = 2;
+        int nOfPages;
+        if (req.getParameter("page") != null) {
+            page = Integer.parseInt(req.getParameter("page"));
+        }
+        int row = OrderDao.getInstance().getOrderCountById(user.getId());
+        List<Order> orders = OrderDao.getInstance().getOrderByUserId(user.getId(), (page - 1) * total + 1, total);
+        nOfPages = row / total;
+        if (row % total > 0) {
+            nOfPages++;
+        }
+
+        req.setAttribute("noOfPages", nOfPages);
+        req.setAttribute("currentPage", page);
+        req.setAttribute("orders", orders);
+        req.getRequestDispatcher("/profile.jsp").forward(req, resp);
     }
 }
